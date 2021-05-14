c948c56c1e7f42f5718c2e405b50a78b995d47f1
LucHoang <hoangsyluc@gmail.com>
2021-05-14 14:36:18 +0700

    Migrate webapp sử dụng Spring Boot

---
+++ src/test/java/com/hsl/migratewebappspringboot/MigrateWebappSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsl.migratewebappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrateWebappSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
